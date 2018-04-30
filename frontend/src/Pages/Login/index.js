7eef808b041353add29978ce0ce79d9becfd65b0
Sarot Busala <sabuza97@gmail.com>
2018-04-30 19:48:41 +0700

    Add .css for Login

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 import axios from 'axios';
 
+import './css/Login.css';
+
 export default class Login extends Component {
 
     state = {
@@ -17,9 +19,21 @@ export default class Login extends Component {
 
     render(){
         return (
-            <div>
-            <div> Login </div>
-            <button className="btn btn-success" onClick={this.props.handleLogin}>Login</button>
+            <div className='flexbox'>
+                <div className='flexbox-container'>
+                    <div> Login </div>
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                        <input id="username" type="text" className="form-control" name="Username" placeholder="Username"></input>
+                    </div>
+                    <div className="input-group">
+                        <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                        <input id="password" type="password" className="form-control" name="password" placeholder="Password"></input>
+                    </div>
+                    <div>
+                    <button className="btn btn-success" onClick={this.props.handleLogin}>Login</button>
+                    </div>
+                </div>
             </div>
         );
         
