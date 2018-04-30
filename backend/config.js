b0708b65fe132a14f3f474668d7330443debe209
Sarot Busala <sabuza97@gmail.com>
2018-04-30 15:42:24 +0700

    Add backend

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+
+const hostname = '127.0.0.1';
+const port = 3001;
+
+//Modules.exports = app;
---
+++ backend/main.js
@@ -0,0 +1,15 @@
+var config = require('./config.js');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+    res.send('WTF');
+});
+
+
+app.listen(3001, () => {
+  console.log(`Server running at port 3001`);
+});
