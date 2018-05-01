ad09e95478de9ada78875cf88e7780777894d63c
Sarot Busala <sabuza97@gmail.com>
2018-05-01 09:22:54 +0700

    Edit Front-end Message state in MessagePanel

--- frontend/src/Pages/ChatApp/MessagePanel/index.js
+++ frontend/src/Pages/ChatApp/MessagePanel/index.js
@@ -13,11 +13,18 @@ export default class MessagePanel extends Component{
     }
 
     render(){
+        console.log(this.state);
         return (
             <div className="message-panel">
                 <div className="row">
                     <div className="col-sm-10">
-                        <input id="messageBox" type="text" className="form-control" name="MessageBox" placeholder="Message . . ." />
+                        <input id="messageBox" type="text" className="form-control" name="MessageBox" 
+                        onChange = { 
+                            (event) => {
+                                this.setState({message : event.target.value});
+                            }
+                        }
+                        placeholder="Message . . ." />
                     </div>
                     <div className="col-sm-2">
                         <button className="btn btn-success" onClick={this.handleSend}>Send</button>
